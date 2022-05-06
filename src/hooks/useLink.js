ca5e5120ff4e70ec6eb007595ebd548fe8e6c2ac
Ruzzlan1 <voramzoram1@gmail.com>
2022-05-06 22:01:20 +0400

    useLink custom hook added for getting url
    i used custom hook for simplify my react component

---
+++ src/hooks/useLink.js
@@ -0,0 +1,25 @@
+import { useQuery } from "react-query";
+
+const API_URL = "https://api.shrtco.de/v2/";
+
+const useLink = (link) => {
+  const fetchUrls = async () => {
+    const shorten = `shorten?url=${link}`;
+    const res = await fetch(`${API_URL}${shorten}`);
+    return res.json();
+  };
+
+  const { data, isLoading, isError, refetch, isRefetching } = useQuery(
+    "links",
+    fetchUrls,
+    {
+      refetchOnWindowFocus: false,
+      enabled: false,
+      refetchOnMount: false,
+    }
+  );
+  console.log(data);
+  return { data, isLoading, isRefetching, isError, refetch };
+};
+
+export default useLink;
