cde7af154d4de6f03b84da87d8b6f6b7543e5416
Ruzzlan1 <voramzoram1@gmail.com>
2022-05-06 20:54:59 +0400

    Adding react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
+import {QueryClientProvider,QueryClient} from 'react-query'
+import Form from "./components/Form";
 
+const queryClient = new QueryClient()
 function App() {
-  return <div className="App">salam</div>;
+  return (
+    <div className="container">
+      <h1>Shorty</h1>
+      <QueryClientProvider client={queryClient}>
+      <Form />
+      </QueryClientProvider>
+    </div>
+  )
 }
 
 export default App;
