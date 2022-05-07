ada12564b331ac788ec70f12c3c88377a749fb6e
Ruzzlan1 <voramzoram1@gmail.com>
2022-05-07 20:53:36 +0400

    better named props

--- src/hooks/useLink.js
+++ src/hooks/useLink.js
@@ -1,25 +1,25 @@
-import { useQuery } from "react-query";
+import {useQuery} from 'react-query'
 
-const API_URL = "https://api.shrtco.de/v2/";
+const API_URL = 'https://api.shrtco.de/v2/'
 
-const useLink = (link) => {
+const useLink = link => {
   const fetchUrls = async () => {
-    const shorten = `shorten?url=${link}`;
-    const res = await fetch(`${API_URL}${shorten}`);
-    return res.json();
-  };
+    const shorten = `shorten?url=${link}`
+    const res = await fetch(`${API_URL}${shorten}`)
+    return res.json()
+  }
 
-  const { data, isLoading, isError, refetch, isRefetching } = useQuery(
-    "links",
+  const {data, isLoading, isError, refetch, isRefetching} = useQuery(
+    'links',
     fetchUrls,
     {
       refetchOnWindowFocus: false,
       enabled: false,
       refetchOnMount: false,
-    }
-  );
-  console.log(data);
-  return { data, isLoading, isRefetching, isError, refetch };
-};
+    },
+  )
+  console.log(data)
+  return {data, isLoading, isRefetching, isError, refetch}
+}
 
-export default useLink;
+export default useLink
