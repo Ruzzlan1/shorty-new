4dd1fc7ea153f63f29c7872b3c34cb87ee232816
Ruzzlan1 <voramzoram1@gmail.com>
2022-05-07 20:55:58 +0400

    footer component created and main styling changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import React from "react";
-import "./App.css";
-import {QueryClientProvider,QueryClient} from 'react-query'
-import Form from "./components/Form";
-
+import React from 'react'
+import './App.css'
+import {QueryClientProvider, QueryClient} from 'react-query'
+import Form from './components/Form'
+import Footer from './components/Footer'
 const queryClient = new QueryClient()
+
 function App() {
   return (
-    <div className="container">
-      <h1>Shorty</h1>
-      <QueryClientProvider client={queryClient}>
-      <Form />
-      </QueryClientProvider>
-    </div>
+    <>
+      <div className="grid h-full place-items-center container mt-20 grid-rows-5 gap-5">
+        <h1 className="font-bold text-4xl">Shorty</h1>
+        <QueryClientProvider client={queryClient}>
+          <Form />
+        </QueryClientProvider>
+        <Footer />
+      </div>
+    </>
   )
 }
 
-export default App;
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='mt-10 row-start-4'>
+        <p className='font-normal font-sans'>Made by <a href="https://github.com/Ruzzlan1"><span className='text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg px-2 py-2 text-sm mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700'>Ruslan Qurbanov .</span></a>
+        Check out the source on <a href="#" className='underline'>Github!</a>
+        </p>
+    </footer>
+  )
+}
+
+export default Footer
+
