8cc821ccb5010a96c489c649a32f61d1c20cc32c
Ruzzlan1 <voramzoram1@gmail.com>
2022-05-11 23:37:13 +0400

    stylings changed and link component rendered

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='mt-10 row-start-4'>
+    <footer className='mt-10 row-start-3'>
         <p className='font-normal font-sans'>Made by <a href="https://github.com/Ruzzlan1"><span className='text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg px-2 py-2 text-sm mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700'>Ruslan Qurbanov .</span></a>
+        </p>
+        <p className='mt-5'>
         Check out the source on <a href="#" className='underline'>Github!</a>
         </p>
     </footer>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,67 +1,64 @@
-import { React, useState } from "react";
-import useLink from "../hooks/useLink";
-import Input from "./Input";
+import {React, useEffect, useState} from 'react'
+import useLink from '../hooks/useLink'
+import Input from './Input'
+import Link from './Link'
 
 const Form = () => {
+
   const [url, setUrl] = useState({
-    fullLink: "",
-  });
+    fullLink: '',
+  })
 
   //? use custom link for getting response
-  const { data, refetch, isRefetching, isLoading, isError } = useLink(
-    url.fullLink
-  );
+  const {data, refetch, isRefetching, isLoading, isError} = useLink(url.fullLink)
 
   //* input change event
-  const onChange = (e) => {
-    const { name, value } = e.target;
-    setUrl((prevUrl) => {
+  const onChange = e => {
+    const {name, value} = e.target
+    setUrl(prevUrl => {
       return {
         ...prevUrl,
         [name]: value,
-      };
-    });
-  };
-
-  //* button.addEventListener()
-  const getUrl = (e) => {
-    e.preventDefault();
-    refetch();
-
-    //* set all links reset again
-    setUrl((prevUrl) => {
-      return {
-        ...prevUrl,
-        fullLink: "",
-        shortLink: "",
-      };
-    });
-  };
-
-  if ((isLoading, isRefetching)) {
-    return <p>Loading...</p>;
+      }
+    })
   }
 
+  //* button.addEventListener()
+  const getUrl = e => {
+    e.preventDefault()
+    refetch()
+    console.log(`data: ${data}`)
+    //* set all links reset again
+    setUrl(prevUrl => {
+      return {
+        ...prevUrl,
+        fullLink: '',
+        shortLink: '',
+      }
+    })
+  }
   if (isError) {
-    return <p>Error...</p>;
+    return <p>Error...</p>
   }
 
   return (
-    <div className="container">
-      <form className="pure-form" onSubmit={getUrl}>
-        <fieldset>
+    <div className="flex flex-col gap-5">
+      <form className="py-4" onSubmit={getUrl}>
+        <fieldset className="md:flex md:gap-4 items-center">
           <Input fullLink={url.fullLink} handleChange={onChange} />
-          <button className="pure-button ml-2">
+          <button className="flex items-center justify-center m-auto p-2 md:px-5 font-bold transition bg-gray-200 border-4 border-black rounded-xl focus:outline-none focus:ring shadow-[6px_6px_0_0_#000] hover:shadow-none active:bg-pink-50 mt-5 md:m-0">
             Give me that god damn short link
           </button>
         </fieldset>
       </form>
-      <p>
-        Data:
-        <a href={`https://${data?.result?.short_link}`}>{data?.result?.short_link}</a>
-      </p>
+      {data && (
+        <Link
+          shortLink={data?.result?.short_link}
+          originalLink={data?.result?.original_link}
+        />
+      )}
     </div>
-  );
-};
+  )
+}
 
-export default Form;
+export default Form
