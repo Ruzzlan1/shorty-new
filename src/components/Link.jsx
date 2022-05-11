7b1fad6e9f44f85a8c4ac44028c1f725305f128f
Ruzzlan1 <voramzoram1@gmail.com>
2022-05-11 23:39:30 +0400

    copy state handled

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,20 +1,43 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import CopyToClipboard from 'react-copy-to-clipboard'
 
+const useTemp = () => {
+  const [copied, setCopied] = useState(false)
+  useEffect(() => {
+    if (copied) {
+      const timer = setTimeout(() => {
+        setCopied(false)
+      }, 2000)
+      return () => clearTimeout(timer)
+    }
+  }, [copied])
+  return [copied, () => setCopied(true)]
+}
+
 const Link = ({shortLink, originalLink}) => {
+  const [copied, update] = useTemp()
   return (
-    <div className="bg-slate-300 p-4 rounded-xl flex items-center justify-between border-gray-300 focus:outline-none focus:ring shadow-[6px_6px_0_0_#666] hover:shadow-none active:bg-pink-50 transition">
-      <p className="font-sans font-normal leading-relaxed text-gray-800 font-base">
-        {originalLink}
-      </p>
-        <p>
-      <CopyToClipboard text={shortLink}>
-          <a className="font-sans font-normal leading-relaxed text-gray-800 font-base cursor-pointer">
-            {shortLink}
-          </a>
-      </CopyToClipboard>
+    <>
+      <div className="bg-slate-300 p-4 rounded-xl flex flex-col lg:flex-row items-center justify-between border-gray-300 focus:outline-none focus:ring shadow-[6px_6px_0_0_#666] hover:shadow-none active:bg-pink-50 transition">
+        <p className="font-sans text-sm font-normal leading-relaxed text-gray-600 font-base">
+          {originalLink}
         </p>
-    </div>
+        <p>
+          <CopyToClipboard text={shortLink} onCopy={() => update()}>
+            <a className="font-sans font-normal text-xl leading-relaxed text-gray-800 font-base cursor-pointer transition hover:scale-150 hover:text-gray-400">
+              {shortLink}
+            </a>
+          </CopyToClipboard>
+        </p>
+      </div>
+      <span
+        className={`text-sm opacity-1 ${
+          !copied && 'opacity-0 duration-500 transition-opacity'
+        }`}
+      >
+        Copied Successfully!
+      </span>
+    </>
   )
 }
 
